0e352bd9488a2db5acc7436d5bf6de38ce49d54d
Paul Grever <paulgrever@gmail.com>
2016-06-06 15:05:53 -0600

    figured out attachements

--- server/slack.js
+++ server/slack.js
@@ -1,18 +1,25 @@
 Slack = {};
 
-Slack.PostMessage = function (options) {
+Slack.PostMessage = function (options, attachments) {
   check(options, Object);
+  check(attachments, Match.Optional([Object]))
   if (options.channel && options.text) {
     const slackPackage = ReactionCore.Collections.Packages.findOne({
       name: 'slack',
       shopId: ReactionCore.getShopId()
     });
     if (slackPackage && slackPackage.enabled) {
-      _.extend(options, {token: slackPackage.settings.api.token});
+      _.extend(options, {
+        token: slackPackage.settings.api.token,
+      });
+      if (attachments) {
+        _.extend(options, {attachments: JSON.stringify(attachments)});
+      }
       HTTP.call('POST',
         'https://slack.com/api/chat.postMessage',
-        { params: options
-      })
+        {
+          params: options,
+        });
       ReactionCore.Log.info(`Slack message posted to ${options.channel}`)
     }
   } else {
