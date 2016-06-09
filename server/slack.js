bfda7ab3dcab59c13d75f4d44cdb4b38d649316d
Paul Grever <paulgrever@gmail.com>
2016-06-09 14:06:12 -0600

    made better logging and quiet fails

--- server/slack.js
+++ server/slack.js
@@ -9,20 +9,30 @@ Slack.PostMessage = function (options, attachments) {
       shopId: ReactionCore.getShopId()
     });
     if (slackPackage && slackPackage.enabled) {
-      _.extend(options, {
-        token: slackPackage.settings.api.token,
-      });
-      if (attachments) {
-        _.extend(options, {attachments: JSON.stringify(attachments)});
-      }
-      HTTP.call('POST',
-        'https://slack.com/api/chat.postMessage',
-        {
-          params: options,
+      if (slackPackage.settings && slackPackage.settings.api && slackPackage.settings.api.token) {
+        _.extend(options, {
+          token: slackPackage.settings.api.token,
         });
-      ReactionCore.Log.info(`Slack message posted to ${options.channel}`)
+        if (attachments) {
+          _.extend(options, {attachments: JSON.stringify(attachments)});
+        }
+        try {
+          HTTP.call('POST',
+            'https://slack.com/api/chat.postMessage',
+            {
+              params: options,
+            });
+          ReactionCore.Log.info(`Slack message posted to ${options.channel}`);
+        } catch (err) {
+          Log.error('Error in making call to API' + err);
+        }
+      } else {
+        ReactionCore.Log.error('Slack is missing it\'s API Token');
+      }
+    } else {
+      ReactionCore.Log.error('Slack package is not enabled.');
     }
   } else {
-    ReactionCore.Log.error('No Channel or Text was sent to Slack');
+    ReactionCore.Log.error('Channel or Text is missing, and preventing Slack Package.');
   }
 }
