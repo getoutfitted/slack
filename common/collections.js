39ba98d4d47cc78351c9bd2848e8aac420c9d6e0
Paul Grever <paulgrever@gmail.com>
2016-08-03 10:42:20 -0600

    updated slack to 14 compatible

--- common/collections.js
+++
@@ -1,9 +0,0 @@
-ReactionCore.Schemas.SlackPackageConfig = new SimpleSchema([
-  ReactionCore.Schemas.PackageConfig, {
-    'settings.api.token': {
-      type: String,
-      label: 'Slack App Token',
-      optional: true
-    }
-  }
-]);
